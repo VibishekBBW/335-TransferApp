5de98b80300489f7079dfcd86927f342e2cb4c82
Vibishek Rathirajan <vibrat@az.zli.ch>
2022-07-08 09:42:52 +0200

    service implemented and binded to Result

--- app/src/main/java/com/example/transferplusapp/BMIService.java
+++ app/src/main/java/com/example/transferplusapp/BMIService.java
@@ -2,15 +2,31 @@ package com.example.transferplusapp;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class BMIService extends Service {
+
+    private final IBinder binder = new BMIBinder();
+
+
+    public class BMIBinder extends Binder{
+        public BMIService getService(){
+            return  BMIService.this;
+        }
+    }
     public BMIService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
     }
+
+    /*
+    public double getBMI(double weight, double height) {
+        return (weight / (height * height)) * 100;
+    }
+     */
 }
--- app/src/main/java/com/example/transferplusapp/ResultActivity.java
+++ app/src/main/java/com/example/transferplusapp/ResultActivity.java
@@ -2,10 +2,13 @@ package com.example.transferplusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -28,6 +31,9 @@ public class ResultActivity extends AppCompatActivity {
     private TextView positionF;
     private TextView bmiF;
 
+    private Double weight;
+    private Double height;
+
     private BMIService bmiService;
     private boolean serviceBound = false;
 
@@ -38,14 +44,41 @@ public class ResultActivity extends AppCompatActivity {
         setUI();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent bindBmiService = new Intent(this, BMIService.class);
+        bindService(bindBmiService, connection, Context.BIND_AUTO_CREATE);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+        serviceBound = false;
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            BMIService.BMIBinder binder = (BMIService.BMIBinder) iBinder;
+            bmiService = binder.getService();
+            serviceBound = true;
+            calculateBMI();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            serviceBound = false;
+        }
+    };
 
     private void getDataFromCamera(){
         Intent caller = getIntent();
         String prename = caller.getStringExtra(vornameIntent1);
         String surname = caller.getStringExtra(nachnameIntent1);
-        Double weight = caller.getDoubleExtra(gewichtIntent1, 80);
-        Double height = caller.getDoubleExtra(groesseIntent1, 1.80);
+        weight = caller.getDoubleExtra(gewichtIntent1, 80);
+        height = caller.getDoubleExtra(groesseIntent1, 1.80);
         String date = caller.getStringExtra(geburtsdatumIntent1);
         String position = caller.getStringExtra(positionIntent1);
         String url = caller.getStringExtra(urlIntent1);
@@ -62,6 +95,22 @@ public class ResultActivity extends AppCompatActivity {
         groesse.setText(height.toString());
         positionF.setText(position);
 
+        /*
+        if(serviceBound == true){
+            calculateBMI();
+        }
+         */
+
+    }
+
+    private void calculateBMI(){
+        if(weight != null && height != null){
+            if(serviceBound){
+                //double bmiValue = bmiService.getBMI(weight, height);
+                //Log.d("tag", "BMIValue: " + bmiValue + " groesse: " + height + " gewicht: " + weight);
+                //bmiF.setText(String.valueOf(bmiValue));
+            }
+        }
     }
 
     private void setImage(Uri rurl){
